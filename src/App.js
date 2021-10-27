ff494a6cb9e3fbb48c53a3c2cbfde560a997ae4c
iflamei <iflamei@mail.ru>
2021-10-27 18:39:23 +0300

    Базовая верстка приложения

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState, useRef } from 'react';
+import * as mobilenet from '@tensorflow-models/mobilenet';
 
 function App() {
+  const [isModelLoading, setIsModelLoading] = useState(false);
+  const [model, setModel] = useState(null);
+  const [imageUrl, setImageUrl] = useState(null);
+
+  const imageRef = useRef();
+
+  const loadModel = async () => {
+    setIsModelLoading(true);
+    try {
+      const model = await mobilenet.load();
+      setModel(model);
+      setIsModelLoading(false);
+    } catch (e) {
+      console.warn(e);
+      setIsModelLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    loadModel();
+  }, []);
+
+  const uploadImage = (e) => {
+    const { files } = e.target;
+    if (files.length > 0) {
+      const url = URL.createObjectURL(files[0]);
+      setImageUrl(url);
+    } else {
+      setImageUrl(null);
+    }
+  };
+
+  if (isModelLoading) {
+    return <h2>Model loading ...</h2>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1 className='header'>Image Identification</h1>
+      <div className='inputHolder'>
+        <input
+          type='file'
+          accept='image/*'
+          capture='camera'
+          className='uploadInput'
+          onChange={uploadImage}
+        />
+      </div>
+      <div className='mainWrapper'>
+        <div className='mainContent'>
+          <div className='imageHolder'>
+            {imageUrl && (
+              <img src={imageUrl} alt='Upload Prev' crossOrigin='anonymous' ref={imageRef} />
+            )}
+          </div>
+        </div>
+        {imageUrl && <button className='button'>Identify Image</button>}
+      </div>
     </div>
   );
 }
